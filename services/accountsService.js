2e4adff91797e864a4b3f79b217a42629889d3d7
Gvantsa Tsotskolauri <gtsotskolauri8@gmail.com>
2022-03-16 16:26:49 +0400

    *

--- services/accountsService.js
+++ services/accountsService.js
@@ -1,93 +1,119 @@
-/* eslint-disable no-restricted-syntax */
-const database = require('../data/database');
+const Account = require('../data/accountsEntity');
 
-exports.getOneAccount = (req, res) => {
-  const accountId = Number(req.params.id);
-  for (const account of database.accounts) {
-    if (account.id === accountId) {
-      res.status(200).json({
-        status: 'success',
-        data: account,
+const successMessage = 'success';
+const errorMessage = 'error';
+
+async function checkRelation(userId, accountId) {
+  const result = await Account.findOne({
+    user_id: userId,
+    _id: accountId,
+  }).exec();
+  return result;
+}
+exports.getOneAccount = async (req, res) => {
+  try {
+    const user = await req.user;
+
+    const ownsAccount = await checkRelation(user.id, req.params.id);
+
+    if (!ownsAccount) {
+      return res.status(404).json({
+        status: errorMessage,
+        message: "Account with given id doesn't exist",
       });
-      return;
     }
-  }
+    const result = await Account.findById(req.params.id);
 
-  res.status(404).json({
-    status: 'error',
-    message: "Account with given id doesn't exist",
-  });
+    res.status(200).json({
+      status: successMessage,
+      data: result,
+    });
+  } catch (err) {
+    return res.status(400).json({ status: errorMessage, message: err.message });
+  }
 };
 
-exports.createAccount = (req, res) => {
-  for (const account of database.accounts) {
-    if (account.name === req.body.name) {
-      res.status(409).json({
-        status: 'error',
-        message: 'Account with given name already exists',
+exports.createAccount = async (req, res) => {
+  try {
+    const user = await req.user;
+    const existingAccount = await Account.findOne({
+      user_id: user.id,
+      title: req.body.title,
+    }).exec();
+
+    if (existingAccount) {
+      return res.status(409).json({
+        status: errorMessage,
+        message: 'Account with given title already exists',
       });
     }
+    const newAccount = new Account({
+      user_id: user.id,
+      ...req.body,
+    });
+
+    await newAccount.save();
+    res.status(201).json({
+      status: successMessage,
+      data: newAccount,
+    });
+  } catch (err) {
+    return res.status(400).json({ status: errorMessage, message: err.message });
   }
-  const newAccount = {
-    id: database.accounts.length + 1,
-    ...req.body,
-  };
-
-  database.accounts.push(newAccount);
-
-  res.status(200).json({
-    status: 'success',
-    data: newAccount,
-  });
 };
 
-exports.updateAccount = (req, res) => {
-  const accountId = Number(req.params.id);
-  let index = 0;
-
-  for (let account of database.accounts) {
-    if (account.id === accountId) {
-      account = {
-        ...account,
-        ...req.body,
-      };
-      database.accounts[index] = account;
-
-      res.status(202).json({
-        status: 'success',
-        data: {
-          message: 'requested account changed successfully',
-        },
+exports.updateAccount = async (req, res) => {
+  try {
+    const user = await req.user;
+    const ownsAccount = await checkRelation(user.id, req.params.id);
+    if (!ownsAccount) {
+      return res.status(409).json({
+        status: errorMessage,
+        message: 'User does not belong requested account',
       });
-      return;
     }
-    index += 1;
-  }
 
-  res.status(404).json({
-    status: 'error',
-    data: {
-      message: 'requested item was not found',
-    },
-  });
+    const updatedAccount = await Account.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json({
+      status: successMessage,
+      data: updatedAccount,
+    });
+  } catch (err) {
+    return res.status(400).json({ status: errorMessage, message: err.message });
+  }
 };
-exports.deleteAccount = (req, res) => {
-  for (let i = 0; i < database.accounts.length; i += 1) {
-    if (database.accounts[i].id === Number(req.params.id)) {
-      database.accounts.splice(i, 1);
-      res.status(200).json({
-        status: 'success',
-        data: {
-          message: 'deleted successfully',
-        },
+
+exports.deleteAccount = async (req, res) => {
+  try {
+    const user = await req.user;
+    const ownsAccount = await checkRelation(user.id, req.params.id);
+    if (!ownsAccount) {
+      return res.status(409).json({
+        status: errorMessage,
+        message: 'User does not belong requested account',
       });
-      return;
     }
+    await Account.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      status: successMessage,
+      data: {
+        message: 'deleted successfully',
+      },
+    });
+  } catch (err) {
+    return res.status(400).json({ status: 'erros', message: err.message });
   }
-  res.status(404).json({
-    status: 'error',
-    data: {
-      message: 'Could not delete account with this id',
-    },
-  });
+};
+exports.getUsersAccounts = async (req, res) => {
+  try {
+    const user = await req.user;
+    const result = await Account.find().where('user_id').equals(user.id).exec();
+    res.status(200).json({ status: successMessage, data: result });
+  } catch (err) {
+    return res.status(400).json({ status: errorMessage, message: err.message });
+  }
 };
